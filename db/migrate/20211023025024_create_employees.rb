53619d8fda7b53b005356cd4880cc63b8cfd25d2
ricardo caceres <ricardocacerez@gmail.com>
2021-10-24 00:19:08 -0300

    Generando modelo empleyee

--- db/migrate/20211023025024_create_employees.rb
+++ db/migrate/20211023025024_create_employees.rb
@@ -3,7 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[6.1]
     create_table :employees do |t|
       t.string :name
       t.string :address
-
+      t.references :manager, index: true   
       t.timestamps
     end
   end
